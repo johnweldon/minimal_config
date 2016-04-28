5aa5ffd3229ea52d95eb13914b1b1b38f65245c7
John Weldon <johnweldon4@gmail.com>
2016-04-28 09:29:40 -0700

    Fix launchpad updates

--- home/bin/update-gopath.sh
+++ home/bin/update-gopath.sh
@@ -7,7 +7,7 @@ for gp in $(echo ${GOPATH} | tr ':' ' '); do
 	for root in ${gp}/src/*; do
 		pth="${root#$gp/src/}/..."
 		echo -e "\n getting ${pth}\n"
-		go get -v -u ${pth} 2>&1 | tee ${tfile}
+		go get -u -insecure ${pth} 2>&1 | tee ${tfile}
 		for bb in $(grep 'ff-only' ${tfile} | sed -e "s/^# cd \(.*\); git pull --ff-only.*$/\1/gp;d"); do
 			pushd ${bb}
 			git co master
