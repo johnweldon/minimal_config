bf3b6f86665ff6d760f87672b310162730eeedd5
John Weldon <johnweldon4@gmail.com>
2016-04-28 10:31:02 -0700

    simpler approach

--- home/bin/update-gopath.sh
+++ home/bin/update-gopath.sh
@@ -2,19 +2,12 @@
 
 tfile=$(mktemp)
 
-for gp in $(echo ${GOPATH} | tr ':' ' '); do
-	echo -e "\nGOPATH ${gp}/src/\n"
-	for root in ${gp}/src/*; do
-		pth="${root#$gp/src/}/..."
-		echo -e "\n getting ${pth}\n"
-		go get -u -insecure ${pth} 2>&1 | tee ${tfile}
-		for bb in $(grep 'ff-only' ${tfile} | sed -e "s/^# cd \(.*\); git pull --ff-only.*$/\1/gp;d"); do
-			pushd ${bb}
-			git co master
-			git pull
-			popd
-		done
-	done
+go get -u -insecure ... 2>&1 | tee ${tfile}
+for bb in $(grep 'ff-only' ${tfile} | sed -e "s/^# cd \(.*\); git pull --ff-only.*$/\1/gp;d"); do
+	pushd ${bb}
+	git co master
+	git pull
+	popd
 done
 
 rm ${tfile}
