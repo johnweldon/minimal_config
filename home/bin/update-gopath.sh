7cfb6305278e7bf23c4f7bb28f8ec95687bcb07a
John Weldon <johnweldon4@gmail.com>
2016-09-06 14:30:20 -0700

    cleanup some more

--- home/bin/update-gopath.sh
+++ home/bin/update-gopath.sh
@@ -4,10 +4,10 @@ tfile=$(mktemp)
 
 go get -u -insecure ... 2>&1 | tee ${tfile}
 for bb in $(grep 'ff-only' ${tfile} | sed -e "s/^# cd \(.*\); git pull --ff-only.*$/\1/gp;d"); do
-	pushd ${bb}
-	git co master
-	git pull
-	popd
+  pushd ${bb}
+  git co master
+  git pull
+  popd
 done
 
 rm ${tfile}
