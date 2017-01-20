28f51c71d2addc58dbd617b799618d50b7722c26
John Weldon <johnweldon4@gmail.com>
2017-01-20 10:27:06 -0700

    ignore import alias warnings

--- home/bin/update-gopath.sh
+++ home/bin/update-gopath.sh
@@ -2,9 +2,10 @@
 
 tfile=$(mktemp)
 
-go get -u -insecure ... 2>&1 | tee ${tfile}
+go get -u -f -insecure ... 2>&1 | tee ${tfile}
 for bb in $(grep 'ff-only' ${tfile} | sed -e "s/^# cd \(.*\); git pull --ff-only.*$/\1/gp;d"); do
   pushd ${bb}
+  echo " *** Attempting to check out master and pull *** "
   git co master
   git pull
   popd
